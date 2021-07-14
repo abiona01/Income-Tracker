59657c186382ec264f1d4867023ab57b7861b706
abiona01 <quadratwemimo@gmail.com>
2021-07-14 23:21:08 +0100

    production

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use("/api/incomes", income);
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	);
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
 	console.log(`Server running on port ${PORT}`.yellow.bold)
