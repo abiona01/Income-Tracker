7067f81adc9b4ef792583040a8867fe8e199d241
abiona01 <quadratwemimo@gmail.com>
2021-07-14 23:36:53 +0100

    postbuild

--- server.js
+++ server.js
@@ -21,12 +21,13 @@ if (process.env.NODE_ENV === "development") {
 
 app.use("/api/incomes", income);
 if (process.env.NODE_ENV === "production") {
+	//Set Static folder
 	app.use(express.static("client/build"));
-	app.get("*", (req, res) =>
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-	);
-}
 
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
 	console.log(`Server running on port ${PORT}`.yellow.bold)
